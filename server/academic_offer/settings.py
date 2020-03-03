2f10fc69532ae48522421e4afd66802a44eadb45
tonyjaimep <tonyjaimep@gmail.com>
2020-03-03 07:35:01 -0600

    rest_framework auth settings

--- server/academic_offer/settings.py
+++ server/academic_offer/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 # REST framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
